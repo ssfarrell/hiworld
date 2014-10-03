0a7c8bda27766cc1d2ebd937b210b20d3d222030
Dan Farrell <dan@farrellit.net>
2014-10-03 17:43:49 -0500

    debugging

--- hiworld.cpp
+++ hiworld.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+string embolden( string s ){
+  string res = new string;
+  res = string.new("\033[0;1m") << s << string.new("\033[0m");
+  return res;
+}
+
 int main()
 {
-	cout << "hello, world" << endl;
+	cout << embolden("Hello") << ", world" << endl;
 	return 0;
 }
